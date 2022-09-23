c932d0276eae9a84019774128c2cdea6461fd7dc
vitor <vitorgilcsgmail.com>
2022-09-23 17:07:02 +0100

    Spring Data JPA Configuration

---
+++ src/main/java/guru/springframework/vgssm/repository/PaymentRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.vgssm.repository;
+
+import guru.springframework.vgssm.domain.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository  extends JpaRepository <Payment, Long>{
+}
